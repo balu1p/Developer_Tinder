b4f9119701600a92f1e039e66effef22955c45d5
Balu Patil <120669013+balu1p@users.noreply.github.com>
2024-11-10 12:40:24 +0530

    Update app.js
    Change route handler

--- src/app.js
+++ src/app.js
@@ -5,16 +5,15 @@ const app = express();
 
 //Request Handler
 
-app.use("/",(req, res)=> {
-    res.send("Hello from Dashboard..!");
-})
-
 app.use("/test",(req, res)=> {
     res.send("Hello from server..!");
 })
 
 app.use("/hello",(req, res)=> {
     res.send("Hello Hello hello..!");
+})
+app.use("/",(req, res)=> {
+    res.send("Hello from Dashboard..!");
 })
 
 app.listen(3000, ()=> {
