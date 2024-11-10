d11ccdeb15cee92d892fd4402a791b2d9d87ce6d
balu-noitavonne <balup@noitavonne.com>
2024-11-10 11:33:21 +0530

    created server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+
+//Request Handler
+
+app.use("/",(req, res)=> {
+    res.send("Hello from Dashboard..!");
+})
+
+app.use("/test",(req, res)=> {
+    res.send("Hello from server..!");
+})
+
+app.use("/hello",(req, res)=> {
+    res.send("Hello Hello hello..!");
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is successfully listening on port 3000");
+});
