e963eb517af316e4af141e7413325851cb778e99
balup <balupatil4815@gmail.com>
2024-11-11 11:49:41 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
 const express = require("express");
 
-const  { authAdmin, authUser } = require('./middlewares/Auth.js');
-
 const app = express();
 
-app.use("/admin", authAdmin);
-
-app.get("/user", authUser, (req, res)=> {
-    // console.log("user auth...");
-    res.send("user.....!");
-})
-
-app.get("/user/login", (req, res)=> {
-    res.send("user login...!");
-})
-
-app.get("/admin/getUserData", (req, res) => {
-  res.send("all user data successfully fetched...");
+app.get("/userData", (req, res)=> {
+    try {
+        throw new Error()
+        res.send("userData....")
+    } catch (err) {
+        console.log(err)
+        res.status(500).send("error happen by mistake call the support team...")
+    }
 });
 
-app.get("/admin/deleteUserData", (req, res) => {
-  res.send("all user data successfully deleted...");
-});
+app.use('/', (err, _, res, next)=> {
+    if(err) {
+        res.status(500).send("Something goes wrong...");
+    }
+})
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
